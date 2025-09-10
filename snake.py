771fd9b2a675d66be8e8351fd6a77c860b04d804
Wu1v2z <artyr.comfy3065@gmail.com>
2025-09-11 01:57:10 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -18,12 +19,43 @@ def game_loop() :
     game_over = False
 
     x1 = dis_width / 2
-    y2 = dis_height / 2
+    y1 = dis_height / 2
 
     x1_change = 0
     y1_change = 0
 
     while not game_over:
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_LEFT:
+                    x1_change = -10
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = 10
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -10
+                elif event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = 10
+
+        x1 += x1_change
+        y1 += y1_change
+
+
+
+        dis.fill(blue)
+        pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+        pygame.display.update()
+
+        time.sleep(0.1)
+
+    pygame.quit()
+    quit()  
 
 game_loop()
