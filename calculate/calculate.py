17485d56f3400969a65cc516a58c55dab6d6c37b
Wu1v2z <artyr.comfy3065@gmail.com>
2025-09-15 02:18:08 +0300

    Update calculate.py

--- calculate/calculate.py
+++ calculate/calculate.py
@@ -2,5 +2,8 @@ Num_1 = float(input('Enter first number: '))
 Num_2 = float(input('Enter second number: '))
 Operation = input('Enter the operation: ')
 
-if Operation == +:
+if Operation == + :
+    print(Num_1, ' + ', Num_2, ' = ', Num_1 + Num_2)     
+elif Operation == - :
+    print(Num_1, ' - ', Num_2, ' = ', Num_1 - Num_2)     
 
