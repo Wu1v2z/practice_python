e6c1e22273e471cd2c8b5f4540cb9fb9113f2f28
Wu1v2z <artyr.comfy3065@gmail.com>
2025-09-14 18:37:30 +0300

    Update gtn.py

--- gues the number/gtn.py
+++ gues the number/gtn.py
@@ -1,1 +1,17 @@
 import random
+import sys
+import time 
+
+
+num = random.randint(1.0, 100.0)
+
+
+while True:
+    gues_num = float(input('Gues the number'))
+
+    if num < gues_num :
+        print('Number is less')
+    elif num > gues_num :
+        print('Number is bigger')
+    else :
+        print('You are winner!') 
